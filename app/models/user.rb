f82f76939f10f373eb7ee0c86d4e4d6f5d97a4f4
Michael Freedman <github@michael-freedman.com>
2014-02-26 22:30:24 +0000

    added second example

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :leads
-  def self.without_leads
+  def self.without_leads_not_exists
     where(<<-SQL)
       NOT EXISTS (SELECT 1 
         FROM   leads_users 
         WHERE  users.id = leads_users.user_id) 
     SQL
+  end
+
+  def self.without_leads_arel_not_in
+    habtm_table = Arel::Table.new(:leads_users)
+    join_table_with_condition = habtm_table.project(habtm_table[:user_id])
+    where(User.arel_table[:id].not_in(join_table_with_condition))
   end
 end
