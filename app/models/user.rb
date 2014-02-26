d0fdc78aa42df0943ba6900b639b3470985d4541
Michael Freedman <github@michael-freedman.com>
2014-02-26 22:15:40 +0000

    example not exists

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :leads
-
+  def self.without_leads
+    where(<<-SQL)
+      NOT EXISTS (SELECT 1 
+        FROM   leads_users 
+        WHERE  users.id = leads_users.user_id) 
+    SQL
+  end
 end
