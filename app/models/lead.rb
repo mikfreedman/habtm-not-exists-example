1def7ae51083fb4dd7aedc8513c5d27f7bb59d30
Michael Freedman <github@michael-freedman.com>
2014-02-26 22:58:02 +0000

    add Lead examples

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,3 +1,26 @@
 class Lead < ActiveRecord::Base
   has_and_belongs_to_many :users
+  def self.without_users
+    where(<<-SQL)
+      NOT EXISTS (SELECT 1 
+        FROM   leads_users 
+        WHERE  leads.id = leads_users.lead_id) 
+    SQL
+  end
+
+  def self.not_connected_to(user)
+    where(<<-SQL, user.id)
+      NOT EXISTS (SELECT 1 
+        FROM   leads_users 
+        WHERE  leads.id = leads_users.lead_id
+        AND leads_users.user_id <> ?
+        ) 
+    SQL
+  end
+
+  def self.without_users
+    habtm_table = Arel::Table.new(:leads_users)
+    join_table_with_condition = habtm_table.project(habtm_table[:lead_id])
+    where(Lead.arel_table[:id].not_in(join_table_with_condition))
+  end
 end
